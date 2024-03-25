002566521ac64421be17e79a9201651e68df89fc
CelioSlomp <slompcelio132@gmail.com>
2024-03-25 18:59:28 -0300

    Adicionado reuso de engenharia de software

--- PadroesProjetos/Lista2/Exercicio1/classes.cpp
+++ PadroesProjetos/Lista2/Exercicio1/classes.cpp
@@ -1,12 +1,59 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-#include "interfaces.cpp"
+#include "interfaces.h"
+
+class WeatherData : public Subject
+{
+public:
+    vector<Observer *> *objetos = {};
+    virtual void registerObserver(Observer *algumDisplay)
+    {
+        objetos->push_back(algumDisplay);
+    }
+    virtual void removeObserver() {}
+    virtual void notifyObservers() {}
+};
 
 class CurrentConditionsDisplay : public Observer, public DisplayElement
 {
 public:
+    string nome;
+
+    CurrentConditionsDisplay(string nome)
+    {
+        this->nome = nome;
+    }
+
     virtual void update()
     {
+        cout << "CurrentConditionsDisplay atualizou\n";
+    }
+
+    virtual void display()
+    {
+        cout << "As medidas atuais são de XX\n";
+    }
+};
+
+class StatisticsDisplay : public Observer, public DisplayElement
+{
+public:
+    string nome;
+
+    StatisticsDisplay(string nome)
+    {
+        this->nome = nome;
+    }
+
+    virtual void update()
+    {
+        cout << "StatisticDisplay atualizou\n";
+    }
+
+    virtual void display()
+    {
+        cout << "As Estatisticas atuais são de YY\n";
     }
 };
