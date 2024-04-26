3fda8c88afb0c5f38af53d616c902429df05fcaa
CelioSlomp <slompcelio132@gmail.com>
2024-04-26 11:09:13 -0300

    Iniciado lista6 paradigmas

--- ParadigmasProg/Lista5/Ex5.hs
+++ ParadigmasProg/Lista5/Ex5.hs
@@ -6,5 +6,5 @@ incremento x = x + 1
 decremento :: (Num a) => a -> a
 decremento x = x - 1
 
-decremento (incremento 9)
-decremento (incremento 9) :: Num a => a -- Resulta em 9.
+-- decremento (incremento 9)
+-- decremento (incremento 9) :: Num a => a -- Resulta em 9.
