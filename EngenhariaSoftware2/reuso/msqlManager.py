9da1c2b16429e3ebee23bdaa2cf24310e178bac7
CelioSlomp <slompcelio132@gmail.com>
2024-05-01 18:46:04 -0300

    melhorado reuso engenhariasoftware2

---
+++ EngenhariaSoftware2/reuso/msqlManager.py
@@ -0,0 +1,17 @@
+import mysql.connector
+import msql
+from xmlp import criarTabela
+
+
+def main():
+    conn = mysql.connector.connect(host="127.0.0.1",       
+                                   user="root",
+                                   password='1234',
+                                   database="bancoDados")
+    
+    cursor = conn.cursor()
+
+    cursor.execute(criarTabela())
+
+if __name__ == "__main__":
+    main()
