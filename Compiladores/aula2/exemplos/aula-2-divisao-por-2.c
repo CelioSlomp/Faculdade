fe8f6d907e6552b60c9e82d5ff55e951e0d8307a
CelioSlomp <slompcelio132@gmail.com>
2024-05-20 16:59:09 -0300

    Melhorado aula2 comp

---
+++ Compiladores/aula2/exemplos/aula-2-divisao-por-2.c
@@ -0,0 +1,23 @@
+// Testar com 100000000 (100 milhões)
+
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+    int i;
+    int casos = atoi(argv[1]);
+    double a;
+
+    for (i = 0; i < casos; i++)
+    {
+        a = 100000000.0;
+        a /= 2.0;
+    }
+
+    printf("%lf\n", a);
+
+    return 0;
+}
