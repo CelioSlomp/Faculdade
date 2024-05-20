34086669fe043a027cb7034d78c5b651bf8a3fd7
CelioSlomp <slompcelio132@gmail.com>
2024-05-20 20:36:30 -0300

    Adicionado aula3 comp

---
+++ Compiladores/aula3/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    // que nojo isso
+
+    int _ = 1;
+    int __ = 2;
+    int ___ = 3;
+    int ____ = 4;
+    int _____ = 5;
+    int ______ = 6;
+
+    printf("%d", _);
+    printf("%d", __);
+    printf("%d", ____);
+    printf("%d", ______);
+
+    return 0;
+}
