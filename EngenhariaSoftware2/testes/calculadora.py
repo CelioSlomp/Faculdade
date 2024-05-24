4aac64c437fa6e1fbe59bf99314bfa92b9856e7b
CelioSlomp <slompcelio132@gmail.com>
2024-05-24 12:20:40 -0300

    adicionado testes es2

--- EngenhariaSoftware2/testes/calculadora.py
+++ EngenhariaSoftware2/testes/calculadora.py
@@ -1,4 +1,3 @@
-
 '''
 Implementar uma calculadora com as seguintes funcionalidades: adição, subtração, multiplicação, divisão, exponenciação e raiz quadrada.
 
@@ -17,16 +16,23 @@ class Calculadora:
     def __init__(self) -> None:
         print("Calculadora iniciada.")
 
-    def adicao(self, *num, **kwarg):
-        if kwarg == []:
-            return num + 0
-        return num + self.adicao(kwarg)
+    def adicao(self, *args):
+        soma = 0
+        for i in args:
+            soma += i
+        return soma
+    
+    def subtracao(self, num1, num2):
+        return num1 - num2
+    
+    def multiplicacao(self, *args):
+        pass
     
 
 def main():
     calc = Calculadora()
 
-    print(calc.adicao(1,2,3,4,5))
+    print(calc.adicao(0, 1, 2, 3))
 
 if __name__ == "__main__":
     main()
