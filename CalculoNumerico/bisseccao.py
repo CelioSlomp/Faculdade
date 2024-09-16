cfd9aa4d5d09629dd241f9f1738ccca68326c015
Celio Ludwig Slomp <slompcelio132@gmail.com>
2024-09-16 17:52:38 -0300

    adicionado calculoNum e GestaoProj

---
+++ CalculoNumerico/bisseccao.py
@@ -0,0 +1,28 @@
+from math import e, cos, sin, log
+
+def f(x):
+    return 3*log(x, e) - x*x/2
+
+def main():
+    a = 1
+    b = 2
+    erro = 0.5*10**(-6)
+    cont = 0
+    xk = (a+b)/2
+    print(f"k={cont} | a={a} | b={b} | xk={xk} | f(a)={f(a):.2f} | f(b)={f(b):.2f} | |b-a|={abs(b-a)}")
+    while abs(b-a) > erro:
+
+        if (f(a)*f(xk) > 0):
+            a = xk
+        else:
+            b = xk
+
+        xk = (a+b)/2
+        cont+=1
+        print(f"k={cont} | a={a} | b={b} | xk={xk} | f(a)={f(a):.2f} | f(b)={f(b):.2f} | |b-a|={abs(b-a)}")
+
+
+
+
+if __name__ == "__main__":
+    main()
