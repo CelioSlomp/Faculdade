9f1307dd9c796cb50578bbd787b07ba81bca593c
Celio Ludwig Slomp <slompcelio132@gmail.com>
2024-09-25 17:50:15 -0300

    testado algo admincomp.zip

---
+++ SegurancaComputacional/aula4/outro.py
@@ -0,0 +1,30 @@
+from zipfile import ZipFile
+import threading
+
+file_name = "teste/HackerSec.zip"
+def thre(x1,x2,x3):
+
+    for x4 in txt:
+        for x5 in txt:
+            for x6 in txt:
+                with ZipFile(file_name, "r") as zip:
+                    senha = x1+x2+x3+x4+x5+x6
+                    try:       
+                        zip.extractall(path="teste", pwd=senha.encode("utf-8"))
+                        exit()
+                    except:
+                        pass
+
+txt = '0123456789'
+for x1 in txt:
+    print("mudou o x1", x1)
+    print("-------------------------------")
+    for x2 in txt:
+        print("mudou o x2", x2)
+        print("-------------------------------")
+        
+        for x3 in txt:
+            print(x3)
+            x = threading.Thread(target=thre, args=(x1,x2,x3))
+            x.start()
+            
--- SegurancaComputacional/aula4/quebrarSenha.py
+++ SegurancaComputacional/aula4/quebrarSenha.py
@@ -1,8 +1,19 @@
-from zipfile import ZipFile 
+from zipfile import ZipFile
+import os
 
-file_name = "admincomp.zip"
+file_name = "teste/admincomp.zip"
 
-with ZipFile(file_name, "r") as zip:
-    senha = ""
-    zip.extractall(path="uncompressed", pwd="".encode("utf-8"))
+x = 0
+while x < 1001:
+
+    with ZipFile(file_name, "r") as zip:
+
+        senha = zip.namelist()[0].split(".")[0]
+
+        zip.extractall(path="teste", pwd=senha.encode("utf-8"))
+        os.remove(file_name)
+        file_name = "teste/" + zip.namelist()[0]
+    
+    print(x)
+    x += 1
     
