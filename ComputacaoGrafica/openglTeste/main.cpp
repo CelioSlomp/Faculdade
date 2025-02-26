3f4b64afed3f9b5d7f0e23986cec81b3d7af3429
Celio Ludwig Slomp <slompcelio132@gmail.com>
2025-02-26 14:14:58 -0300

    testado triangulo opengl

--- ComputacaoGrafica/openglTeste/main.cpp
+++ ComputacaoGrafica/openglTeste/main.cpp
@@ -41,19 +41,37 @@ int main()
     //--------------------------------
 
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f};
+        0.9f, 0.9f, 0.0f,   // top right
+        -0.9f, -0.9f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, // bottom left
+        0.9f, -0.9f, 0.0f   // top left
+    };
+    unsigned int indices[] = {
+        // note that we start from 0!
+        0, 1, 3 // first triangle
+        // 1, 2, 3  // second triangle
+    };
+
+    // Creating a Vertex Array Object (VAO)
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
 
     // VBO means Vertex Buffer Objects, that makes possible to send all the data to the
     // GPU memory. Sending objects from CPU to GPU is very slow, then we send all the data
     // at once to the GPU. After that, the vertex can access that data very fast.
     unsigned int VBO;
     glGenBuffers(1, &VBO);
-
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
     // After defining the buffer, we should send all the data inside vertices to the buffer
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Each vertex has a 12 byte size
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // We can compile this GLSL only once
@@ -108,26 +126,12 @@ int main()
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    // Each vertex has a 12 byte size
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-    glEnableVertexAttribArray(0);
+    // EBO
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
 
-    // Creating a Vertex Array Object (VAO)
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-
-    glBindVertexArray(VAO);
-    // 2. copy our vertices array in a buffer for OpenGL to use
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    // 3. then set our vertex attributes pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-    glEnableVertexAttribArray(0);
-
-    // ..:: Drawing code (in render loop) :: ..
-    // 4. draw the object
-    // glUseProgram(shaderProgram);
-    glBindVertexArray(VAO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     //--------------------------------
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -136,13 +140,19 @@ int main()
     {
         processInput(window);
 
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
 
     glfwTerminate();
     return 0;
