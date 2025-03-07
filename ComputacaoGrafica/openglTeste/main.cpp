d73b9137b4f99ebded1e170255ca9db19e79ce4e
Celio Ludwig Slomp <slompcelio132@gmail.com>
2025-03-07 16:34:45 -0300

    testado algo em CG

--- ComputacaoGrafica/openglTeste/main.cpp
+++ ComputacaoGrafica/openglTeste/main.cpp
@@ -41,15 +41,15 @@ int main()
     //--------------------------------
 
     float vertices[] = {
-        0.9f, 0.9f, 0.0f,   // top right
-        -0.9f, -0.9f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        0.9f, -0.9f, 0.0f   // top left
+        0.2f, 0.2f, 0.0f,   // top right
+        -0.2f, 0.2f, 0.0f,  // bottom right
+        -0.2f, -0.2f, 0.0f, // bottom left
+        0.2f, -0.2f, 0.0f   // top left
     };
     unsigned int indices[] = {
         // note that we start from 0!
-        0, 1, 3 // first triangle
-        // 1, 2, 3  // second triangle
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
     };
 
     // Creating a Vertex Array Object (VAO)
